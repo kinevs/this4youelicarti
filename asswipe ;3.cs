f2afda22098ff3f7a942e66dbfe58d6aaaa40002
kinevs <145731349+kinevs@users.noreply.github.com>
2023-12-08 20:07:53 -0500

    Add files via upload

---
+++ asswipe ;3.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.Title = "this for you eli";
+Console.WriteLine("Is Carti Droppin?");
+
+Console.ReadLine();
+
+Console.WriteLine("no bitch ass nigga"); 
+
+Console.ReadKey();
